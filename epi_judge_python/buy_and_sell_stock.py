50ec229b68eaf181579dc5f29003ef63f4b8d685
Mlkz <maho.mamulashvili@gmail.com>
2024-05-09 10:40:16 -0400

    find longest sub array

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -14,8 +14,24 @@ def buy_and_sell_stock_once(prices: List[float]) -> float:
 
 
 
+def find_longest_sub_array(nums: List[int]) -> int:
+    if not nums:
+        return 0
 
+    max_seq = 1
+    current_seq = 1
 
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            current_seq += 1
+        else:
+            max_seq = max(max_seq, current_seq)
+            current_seq = 1
+
+    return max_seq if max_seq >= current_seq else current_seq
+
+A = [1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 2]
+print(find_longest_sub_array(A))
 
 
 if __name__ == '__main__':
