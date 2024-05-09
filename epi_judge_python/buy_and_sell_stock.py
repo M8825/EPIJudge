6a903d1925f9f534be03492ebdc19f4f855a7677
Mlkz <maho.mamulashvili@gmail.com>
2024-05-09 10:08:09 -0400

    buy sell stock one

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,10 +2,20 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    min_price, max_profit = float('inf'), 0.0
+
+    for price in prices:
+        sell_today = price - min_price
+        max_profit = max(max_profit, sell_today)
+        min_price = min(min_price, price)
+
+    return max_profit
+
+
+
+
+
 
 
 if __name__ == '__main__':
